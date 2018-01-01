61b9a5b4b6d000dca21d6685bb3ec6d3ca4115ba
Snehal Shekatkar <snehalshekatkar@gmail.com>
2018-01-01 23:30:20 +0530

    Edits

--- Lecture2/Algorithms/girvan_newman_gt.py
+++ Lecture2/Algorithms/girvan_newman_gt.py
@@ -1,14 +1,34 @@
 import graph_tool.all as gt
+import pandas as pd
 
-g = gt.collection.data['karate']
+#g = gt.collection.data['karate']
+
+df = pd.read_csv('edge_list.dat', header = None)
+edges = []
+
+for i in range(len(df)):
+    edges.append((df.iloc[i][0], df.iloc[i][1]))
+
+g = gt.Graph(directed = False)
+
+name = g.add_edge_list(edges, hashed = True)
+
 
 #gt.graph_draw(g, vertex_text = g.vertex_index)
 pos = gt.sfdp_layout(g)
 
-for _ in range(g.num_edges()):
+ind = 0
+gt.graph_draw(g, pos = pos, vertex_shape = 'double_circle', vertex_text = name, vertex_font_size = 8, vertex_text_position = -2, vertex_fill_color = '#729fcf', vertex_pen_width = 3, edge_pen_width = bt, output = 'gn{}.pdf'.format(ind))
+
+for ind in range(g.num_edges()):
+
+    bt = gt.betweenness(g)[1]
+    for e in g.edges():
+        bt[e] = 20 * bt[e]
+
     # Calculate edge betweenness
     ebt = gt.betweenness(g)[1]
     d = {e : ebt[e] for e in g.edges()}
     e = max(d, key = d.get)
     g.remove_edge(e)
-    gt.graph_draw(g, pos = pos)
+    gt.graph_draw(g, pos = pos, vertex_shape = 'double_circle', vertex_text = name, vertex_font_size = 8, vertex_text_position = -2, vertex_fill_color = '#729fcf', vertex_pen_width = 3, edge_pen_width = bt, output = 'gn{}.pdf'.format(ind+1))
